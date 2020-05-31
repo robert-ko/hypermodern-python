91feda2595251fe145df052ad605bda9ce1b8d0b
Robert <robert@MacBook-Pro.local>
2020-05-31 08:57:34 -0700

    add noxfile and wikipedia

---
+++ src/hypermodern_python/wikipedia.py
@@ -0,0 +1,13 @@
+# src/hypermodern-python/wikipedia.py
+import requests
+
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+def random_page():
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        return response.json()
+
+
